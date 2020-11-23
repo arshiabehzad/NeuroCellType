4ba8ebb3d1d4ea83c8c3c362ddfae02c6da62129
arshiabehzad <44649046+arshiabehzad@users.noreply.github.com>
2020-11-23 12:54:27 -0800

    added main function and dropped NAs

--- main.py
+++ main.py
@@ -1,5 +1,13 @@
 import pandas as pd
 
-df = pd.read_csv("cell_types_specimen_details.csv")
-new_df = df.filter(['specimen__id','specimen__hemisphere','structure__name','ef__avg_firing_rate','tag__dendrite_type','structure__name', 'donor__species'])
-print(new_df.head())
+
+def main():
+    df = pd.read_csv("cell_types_specimen_details.csv")
+    new_df = df.filter(['specimen__id','specimen__hemisphere','structure__name','ef__avg_firing_rate','tag__dendrite_type', 'donor__species'])
+    new_df.dropna(inplace = True)
+    print(new_df.head())
+    new_df.info()
+
+if __name__ == '__main__':
+    main()
+
